063a7581328aadd4e10667f84b5d7014408c094e
vnijs <vnijs@ucsd.edu>
2020-10-25 06:05:58 +0000

    update

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 rm vsix_list.txt
 touch vsix_list.txt
 vsix_list=$(ls -d *.vsix 2>/dev/null)
+echo $vsix_list
 for i in ${vsix_list}; do
-  echo "https://github.com/radiant-rstats/docker-vsix-dev/raw/master/${i}" >> ../docker-vsix-dev/vsix_list.txt
+  echo "https://github.com/radiant-rstats/docker-vsix-dev/raw/main/${i}" >> ../docker-vsix-dev/vsix_list.txt
 done
 
 git add .
